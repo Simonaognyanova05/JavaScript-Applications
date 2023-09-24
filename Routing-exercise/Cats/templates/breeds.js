fdd88e44836fa38f47f34ad9bea36460d7448cb3
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-09-24 11:26:26 +0300

    Add server request for breeds

--- Routing-exercise/Cats/templates/breeds.js
+++ Routing-exercise/Cats/templates/breeds.js
@@ -1,12 +1,21 @@
 import { html, render } from '../node_modules/lit-html/lit-html.js';
 
-const breedsTemplate = () => html`
+const getBreeds = () => 
+    fetch('http://localhost:3030/jsonstore/cats')
+    .then(res => res.json())
+    .then(result => Object.values(result))
+    .catch(err => alert(err));
+
+const breedsTemplate = (breeds) => html`
 <h3>Cats' Breeds</h3>
 <ul>
-   <li>Persian cat</li>
+   ${breeds.map(x => html`<li>${x.breed}</li>`)}
 </ul>
 `;
 
 export const breedsView = () => {
-    render(breedsTemplate(), document.querySelector('#root'));
+    getBreeds()
+    .then(breeds => {
+        render(breedsTemplate(breeds), document.querySelector('#root'));
+    })
 }
