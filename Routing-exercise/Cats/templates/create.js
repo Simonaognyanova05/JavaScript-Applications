cd19ac725099244341da8a2a8a8761e16ed74adb
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-09-24 11:52:31 +0300

    Add form for creating new cat

--- Routing-exercise/Cats/templates/create.js
+++ Routing-exercise/Cats/templates/create.js
@@ -1,8 +1,25 @@
 import { html, render } from '../node_modules/lit-html/lit-html.js';
 
+const createCat = (e) => {
+    e.preventDefault();
+    let formData = new FormData(e.currentTarget);
+
+    let name = formData.get('name');
+    let breed = formData.get('breed');
+    let age = formData.get('age');
+
+    fetch('http://localhost:3030/jsonstore/cats', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json', // Set the content type to JSON
+        },
+        body: JSON.stringify({name, breed, age})
+    })
+    .then(() => alert('Successfull request'))
+}
 const createTemplate = () => html`
 <h3>Create new cat</h3>
-<form>
+<form @submit=${createCat}>
 <input type="text" placeholder="Name of cat" name="name">
 <input type="text" placeholder="Breed of cat" name="breed">
 <input type="text" placeholder="Age of cat" name="age">
