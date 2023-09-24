77f565877ab5162dc8f5cb090168e28e06528e87
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-09-24 11:17:04 +0300

    Add server request for list of names

--- Routing-exercise/Cats/templates/names.js
+++ Routing-exercise/Cats/templates/names.js
@@ -1,12 +1,21 @@
 import { html, render } from '../node_modules/lit-html/lit-html.js';
 
-const namesTemplate = () => html`
+const getNames = () => 
+   fetch('http://localhost:3030/jsonstore/cats')
+   .then(res => res.json())
+   .then(result => Object.values(result));
+
+
+const namesTemplate = (names) => html`
 <h3>Cats' names</h3>
 <ul>
-<li>Goshko</li>
+${names.map(x => html`<li>${x.name}</li>`)}
 </ul>
 `;
 
 export const namesView = () => {
-    render(namesTemplate(), document.querySelector('#root'));
+    getNames()
+    .then(names => {
+        render(namesTemplate(names), document.querySelector('#root'));
+    })
 }
