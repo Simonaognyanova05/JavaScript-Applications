05fd5e0f5369d81ffb8f19d92ab8a7c3a77e3147
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-09-25 21:02:05 +0300

    Add libreies into individual file

--- Routing-exercise/01.Furniture/templates/home.js
+++
@@ -1,8 +0,0 @@
-import { html, render } from '../node_modules/lit-html/lit-html.js';
-
-const homeTemplate = () => html`
-`;
-
-export const homeView = () => {
-    render(homeTemplate(), document.querySelector('#root'));
-}
