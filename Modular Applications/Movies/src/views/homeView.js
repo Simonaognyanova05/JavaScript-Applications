021a3c2b002688c936b0aa22c9ee1168581ee249
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-09-27 20:48:57 +0300

    Add template for homeView

--- Modular Applications/Movies/src/views/homeView.js
+++ Modular Applications/Movies/src/views/homeView.js
@@ -1,9 +1,25 @@
 import { html, render } from '../../node_modules/lit-html/lit-html.js';
+import * as movieService from '../services/movieService.js';
 
-const homeTamplate = () => html`
-<h1>Home Page</h1>
+const movieTemplate = (movie) => html`
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${movie.title}</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+`;
+const homeTamplate = (movies) => html`
+<h1>Movie List</h1>
+
+${movies.map(x => movieTemplate(x))};
 `;
 
 export const homeView = (ctx) => {
-    render(homeTamplate(), document.querySelector('#root'));
+    movieService.getAll()
+    .then(movies => {
+        render(homeTamplate(movies), document.querySelector('#root'));
+    })
 }
