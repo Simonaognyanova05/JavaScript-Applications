725be409aaca692e33bfd5874b4703df16c00ef7
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-09-27 21:10:50 +0300

    Ready homeView

--- Modular Applications/Movies/src/views/homeView.js
+++ Modular Applications/Movies/src/views/homeView.js
@@ -3,10 +3,9 @@ import * as movieService from '../services/movieService.js';
 
 const movieTemplate = (movie) => html`
 <div class="card movie-card">
-  <img src="..." class="card-img-top" alt="...">
+  <img src=${movie.img} class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${movie.title}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
