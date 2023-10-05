73b1670b930d3d59009456d10a89efb60968e2b2
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-05 20:39:40 +0300

    Add permanent navbar

---
+++ JS Applications Retake Exam - 8 August 2023/Samurider/src/templates/navbar.js
@@ -0,0 +1,27 @@
+import { html, render } from '../../node_modules/lit-html/lit-html.js';
+
+const loggedUsers = () => html`
+<div class="user">
+<a href="#">Add Motorcycle</a>
+<a href="#">Logout</a>
+</div>
+`;
+
+const guestUsers = () => html`
+<div class="guest">
+            <a href="#">Login</a>
+            <a href="#">Register</a>
+            
+          </div>
+`;
+const permanentNav = () => html`
+<div>
+    <a href="test.html">Motorcycles</a>
+    <a href="#">Search</a>
+</div>  
+`;
+
+export const navView = () => {
+    const navElement = document.querySelector('nav');
+    render(permanentNav(), navElement)
+}
