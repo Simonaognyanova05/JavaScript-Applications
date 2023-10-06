2c5162852c9bca5821f2cbade26b4902a26e2f1d
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-06 12:58:30 +0300

    Dashboard template

--- JS Applications Retake Exam - 8 August 2023/Samurider/src/app.js
+++ JS Applications Retake Exam - 8 August 2023/Samurider/src/app.js
@@ -1,5 +1,9 @@
 import page from "../node_modules/page/page.mjs";
+import { dashboardView } from "./templates/dashboard.js";
+import { homeView } from "./templates/homePage.js";
 import { navbarView } from "./templates/navbar.js";
 
 navbarView();
+page('/', homeView);
+page('dashboard', dashboardView);
 page.start();
