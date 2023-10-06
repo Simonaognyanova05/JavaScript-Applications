c6d70d2645868fc955452256f70b869c6b912b0b
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-06 13:02:54 +0300

    Add register template

--- JS Applications Retake Exam - 8 August 2023/Samurider/src/app.js
+++ JS Applications Retake Exam - 8 August 2023/Samurider/src/app.js
@@ -3,10 +3,13 @@ import { dashboardView } from "./templates/dashboard.js";
 import { homeView } from "./templates/homePage.js";
 import { navbarView } from "./templates/navbar.js";
 import { noUsersView } from "./templates/noUsers.js";
+import { registerView } from "./templates/register.js";
 
 navbarView();
 page('/', homeView);
 page('/dashboard', dashboardView);
 page('/nousers', noUsersView);
+page('/register', registerView);
+
 
 page.start();
---
+++ JS Applications Retake Exam - 8 August 2023/Samurider/src/templates/register.js
@@ -0,0 +1,36 @@
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+
+const registerTemplate = () => html`
+<section id="register">
+          <div class="form">
+            <h2>Register</h2>
+            <form class="register-form">
+              <input
+                type="text"
+                name="email"
+                id="register-email"
+                placeholder="email"
+              />
+              <input
+                type="password"
+                name="password"
+                id="register-password"
+                placeholder="password"
+              />
+              <input
+                type="password"
+                name="re-password"
+                id="repeat-password"
+                placeholder="repeat password"
+              />
+              <button type="submit">register</button>
+              <p class="message">Already registered? <a href="#">Login</a></p>
+            </form>
+          </div>
+        </section>
+`;
+
+export const registerView = () => {
+    document.querySelector('main').innerHTML = '';
+    render(registerTemplate(), document.querySelector('main'));
+}
