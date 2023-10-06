4f5146ba59bdac55febb195d32e613d2454f3060
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-06 12:28:54 +0300

    Resources for task

--- JS Applications Retake Exam - 8 August 2023/Samurider/src/app.js
+++
@@ -1,9 +0,0 @@
-import page from '../node_modules/page/page.mjs';
-import { homePageView } from './templates/homePage.js';
-import { loginView } from './templates/login.js';
-import { navView } from './templates/navbar.js';
-
-navView();
-page('/', homePageView);
-page('/login', loginView);
-page.start();
--- JS Applications Retake Exam - 8 August 2023/Samurider/src/templates/available.js
+++
@@ -1,5 +0,0 @@
-import { html, render } from "../../node_modules/lit-html/lit-html.js";
-
-const availableTemplate = () => html`
-
-`;
