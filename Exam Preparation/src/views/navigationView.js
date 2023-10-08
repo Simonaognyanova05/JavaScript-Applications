84149a57cff189f95f2d4fd0af7defce8213dc7c
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-08 11:27:05 +0300

    Split navigation by user

--- Exam Preparation/src/views/navigationView.js
+++ Exam Preparation/src/views/navigationView.js
@@ -1,6 +1,18 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
-const navigationTemplate = () => html`
+const guestLinks = html`
+<!--Only guest-->
+<li><a href="#">Login</a></li>
+<li><a href="#">Register</a></li>
+`;
+
+const userLinks = html`
+<!--Only user-->
+    <li><a href="#">Create Album</a></li>
+    <li><a href="#">Logout</a></li>
+`;
+
+const navigationTemplate = (isAuthenticated) => html`
 <nav>
     <img src="./images/headphones.png">
     <a href="#">Home</a>
@@ -8,12 +20,9 @@ const navigationTemplate = () => html`
         <!--All user-->
         <li><a href="#">Catalog</a></li>
         <li><a href="#">Search</a></li>
-        <!--Only guest-->
-        <li><a href="#">Login</a></li>
-        <li><a href="#">Register</a></li>
-        <!--Only user-->
-        <li><a href="#">Create Album</a></li>
-        <li><a href="#">Logout</a></li>
+        ${isAuthenticated 
+          ? userLinks
+          : guestLinks}
     </ul>
 </nav>
 `;
