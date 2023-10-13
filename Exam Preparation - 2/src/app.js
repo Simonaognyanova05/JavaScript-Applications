c496c50a3c0ba9e834ebfd75a1d47f8e3dfce3e3
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-13 20:45:24 +0300

    Add navigation template

---
+++ Exam Preparation - 2/src/app.js
@@ -0,0 +1,5 @@
+import page from '../node_modules/page/page.mjs';
+import { renderMiddleware } from './middlewares/renderMiddleware.js';
+
+page(renderMiddleware);
+page.start();
---
+++ Exam Preparation - 2/src/middlewares/renderMiddleware.js
@@ -0,0 +1,10 @@
+import { render } from "../../node_modules/lit-html/lit-html.js";
+import { navigationView } from "../views/navigationView.js";
+
+
+let headerElement = document.querySelector('.header-navigation');
+
+export const renderMiddleware = (ctx, next) => {
+    render(navigationView(ctx), headerElement);
+    next();
+}
