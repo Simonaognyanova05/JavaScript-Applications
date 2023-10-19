7a74854f538c06990b62090162835074cd79b66d
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-19 08:34:38 +0300

    Add edit template

--- Exam Preparation - 2/src/app.js
+++ Exam Preparation - 2/src/app.js
@@ -2,6 +2,7 @@ import page from "../node_modules/page/page.mjs";
 
 import { renderMiddleware } from "./middlewares/renderMiddleware.js";
 import { createView } from "./views/create.js";
+import { editView } from "./views/edit.js";
 import { homeView } from "./views/home.js";
 import { loginView } from "./views/login.js";
 import { registerView } from "./views/register.js";
@@ -11,6 +12,6 @@ page('/', homeView);
 page('/login', loginView);
 page('/register', registerView);
 page('/create', createView);
-
+page('/edit', editView);
 
 page.start();
