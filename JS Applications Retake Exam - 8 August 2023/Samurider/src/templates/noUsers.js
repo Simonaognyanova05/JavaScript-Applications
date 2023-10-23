82584310be5e7a542df5963230c9916ee5297a8c
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-23 21:14:27 +0300

    Preparation for new task

--- JS Applications Retake Exam - 8 August 2023/Samurider/src/templates/noUsers.js
+++
@@ -1,9 +0,0 @@
-import { html, render } from '../../node_modules/lit-html/lit-html.js';
-
-const noUsersTemplate = () => html`
-<h2 class="no-avaliable">No avaliable motorcycles yet.</h2>
-`;
-
-export const noUsersView = () => {
-    render(noUsersTemplate(), document.querySelector('main'));
-}
