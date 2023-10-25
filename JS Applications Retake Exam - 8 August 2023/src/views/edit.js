c3c1891ce23f44c410aa05471a23d32368dda7fe
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-25 21:15:17 +0300

    Add values in the places for edit page

--- JS Applications Retake Exam - 8 August 2023/src/views/edit.js
+++ JS Applications Retake Exam - 8 August 2023/src/views/edit.js
@@ -1,18 +1,18 @@
 import { html, render } from '../../node_modules/lit-html/lit-html.js';
-import { edit } from '../services/requests.js';
+import { edit, getOne } from '../services/requests.js';
 
-const editTemplate = (submitHandler) => html`
+const editTemplate = (motor, submitHandler) => html`
     <section id="edit">
         <h2>Edit Motorcycle</h2>
         <div class="form">
           <h2>Edit Motorcycle</h2>
           <form class="edit-form" @submit=${submitHandler}>
-            <input type="text" name="model" id="model" placeholder="Model" />
-            <input type="text" name="imageUrl" id="moto-image" placeholder="Moto Image" />
-            <input type="number" name="year" id="year" placeholder="Year" />
-            <input type="number" name="mileage" id="mileage" placeholder="mileage" />
-            <input type="number" name="contact" id="contact" placeholder="contact" />
-            <textarea id="about" name="about" placeholder="about" rows="10" cols="50"></textarea>
+            <input type="text" name="model" id="model" value=${motor.model}/>
+            <input type="text" name="imageUrl" id="moto-image" value=${motor.imageUrl} />
+            <input type="number" name="year" id="year" value=${motor.year} />
+            <input type="number" name="mileage" id="mileage" value=${motor.mileage} />
+            <input type="number" name="contact" id="contact" value=${motor.contact} />
+            <textarea id="about" name="about" rows="10" cols="50">${motor.about}</textarea>
             <button type="submit">Edit Motorcycle</button>
           </form>
         </div>
@@ -31,5 +31,9 @@ export const editView = (ctx) => {
     })
     console.log(data);
   }
-  render(editTemplate(submitHandler), document.querySelector('main'));
+  getOne(ctx.params.motorId)
+  .then(motor => {
+
+    render(editTemplate(motor, submitHandler), document.querySelector('main'));
+  })
 }
