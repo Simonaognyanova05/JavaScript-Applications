d1367a8008235a29716eff956f6d8f5a5633eefa
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-26 08:44:11 +0300

    Add details template

--- JS Applications Exam - 29 July 2023/src/app.js
+++ JS Applications Exam - 29 July 2023/src/app.js
@@ -2,6 +2,7 @@ import page from "../node_modules/page/page.mjs";
 import { renderNavigationMiddleware } from "./middlewares/renderMiddlewares.js";
 import { createView } from "./views/create.js";
 import { dashboardView } from "./views/dashboard.js";
+import { detailsView } from "./views/details.js";
 import { editView } from "./views/edit.js";
 import { homeView } from "./views/home.js";
 import { loginView } from "./views/login.js";
@@ -14,5 +15,7 @@ page('/register', registerView);
 page('/login', loginView);
 page('/create', createView);
 page('/edit', editView);
+page('/details', detailsView);
+
 
 page.start();
