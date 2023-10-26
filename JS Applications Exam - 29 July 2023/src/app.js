1a862615456d2587fbdc15bfc070872f79146d4b
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-26 08:25:10 +0300

    Add home template

--- JS Applications Exam - 29 July 2023/src/app.js
+++ JS Applications Exam - 29 July 2023/src/app.js
@@ -1,5 +1,7 @@
 import page from "../node_modules/page/page.mjs";
 import { renderNavigationMiddleware } from "./middlewares/renderMiddlewares.js";
+import { homeView } from "./views/home.js";
 
 page(renderNavigationMiddleware);
+page('/', homeView);
 page.start();
