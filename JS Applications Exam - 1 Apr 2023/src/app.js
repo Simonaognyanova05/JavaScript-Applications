864aa03db98b4690b28aaa034005339f0eaf3063
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-28 13:03:00 +0300

    Add home view

--- JS Applications Exam - 1 Apr 2023/src/app.js
+++ JS Applications Exam - 1 Apr 2023/src/app.js
@@ -1,6 +1,8 @@
 import page from "../node_modules/page/page.mjs";
 import { renderNavigation } from "./middlewares/renderMiddleware.js";
+import { homeView } from "./views/home.js";
 
 
 page(renderNavigation);
+page('/', homeView);
 page.start();
---
+++ JS Applications Exam - 1 Apr 2023/src/views/home.js
@@ -0,0 +1,12 @@
+import { html, render } from "../../node_modules/lit-html/lit-html.js";
+
+const homeTemplate = () => html`
+<section id="home">
+    <h1>Learn more about your favorite fruits</h1>
+    <img src="./images/pexels-pixabay-161559-dImkWBDHz-transformed (1).png" alt="home" />
+</section>
+`;
+
+export const homeView = (ctx) => {
+    render(homeTemplate(), document.querySelector('main'));
+}
