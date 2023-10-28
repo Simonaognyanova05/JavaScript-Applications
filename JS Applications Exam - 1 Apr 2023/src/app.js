b7e3c5a8a8410b919fcdcb0d869d90f26a4f71a7
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-28 13:16:52 +0300

    Add edit view

--- JS Applications Exam - 1 Apr 2023/src/app.js
+++ JS Applications Exam - 1 Apr 2023/src/app.js
@@ -2,6 +2,7 @@ import page from "../node_modules/page/page.mjs";
 import { renderNavigation } from "./middlewares/renderMiddleware.js";
 import { createView } from "./views/create.js";
 import { dashboardView } from "./views/dashboard.js";
+import { editView } from "./views/edit.js";
 import { homeView } from "./views/home.js";
 import { loginView } from "./views/login.js";
 import { registerView } from "./views/register.js";
@@ -13,7 +14,6 @@ page('/dashboard', dashboardView);
 page('/register', registerView);
 page('/login', loginView);
 page('/create', createView);
-
-
+page('/edit', editView);
 
 page.start();
