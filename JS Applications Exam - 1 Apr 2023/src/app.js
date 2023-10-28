e115f249e48c7b468108eb4d5e536fbd40e2328d
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-28 13:07:49 +0300

    Add register view

--- JS Applications Exam - 1 Apr 2023/src/app.js
+++ JS Applications Exam - 1 Apr 2023/src/app.js
@@ -2,10 +2,13 @@ import page from "../node_modules/page/page.mjs";
 import { renderNavigation } from "./middlewares/renderMiddleware.js";
 import { dashboardView } from "./views/dashboard.js";
 import { homeView } from "./views/home.js";
+import { registerView } from "./views/register.js";
 
 
 page(renderNavigation);
 page('/', homeView);
 page('/dashboard', dashboardView);
+page('/register', registerView);
+
 
 page.start();
