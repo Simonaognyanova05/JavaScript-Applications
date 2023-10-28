6ebca2d4e9777e9e348e0d9cabe0b7f591f23641
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-28 13:10:28 +0300

    Add login view

--- JS Applications Exam - 1 Apr 2023/src/app.js
+++ JS Applications Exam - 1 Apr 2023/src/app.js
@@ -2,6 +2,7 @@ import page from "../node_modules/page/page.mjs";
 import { renderNavigation } from "./middlewares/renderMiddleware.js";
 import { dashboardView } from "./views/dashboard.js";
 import { homeView } from "./views/home.js";
+import { loginView } from "./views/login.js";
 import { registerView } from "./views/register.js";
 
 
@@ -9,6 +10,8 @@ page(renderNavigation);
 page('/', homeView);
 page('/dashboard', dashboardView);
 page('/register', registerView);
+page('/login', loginView);
+
 
 
 page.start();
