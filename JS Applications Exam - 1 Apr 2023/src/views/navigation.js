491b7324e7c02e8e74ce5cdb52e80158be54a8be
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-28 12:59:44 +0300

    Add navigation view

---
+++ JS Applications Exam - 1 Apr 2023/src/views/navigation.js
@@ -0,0 +1,32 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+
+const loggedUser = html`
+<div class="user">
+    <a href="#">Add Fruit</a>
+    <a href="#">Logout</a>
+</div>
+`;
+
+const guestUser = html`
+<div class="guest">
+    <a href="#">Login</a>
+    <a href="#">Register</a>        
+</div>
+`;
+const navigationTemplate = (user) => html`
+<a id="logo" href="/">
+    <img id="logo-img" src="./images/logo.png" alt=""/>
+</a>
+
+<nav>
+    <div>
+        <a href="#">Fruits</a>
+        <a href="#">Search</a>
+    </div>
+    ${user ? loggedUser : guestUser}          
+</nav>
+`;
+
+export const navigationView = (ctx) => {
+    return navigationTemplate(ctx.user);
+}
