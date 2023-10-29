2e25ac2d8cb295b6bbec74ac94983c644b39d3cf
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-10-29 07:01:03 +0200

    Add login request

--- JS Applications Exam - 1 Apr 2023/src/views/login.js
+++ JS Applications Exam - 1 Apr 2023/src/views/login.js
@@ -1,10 +1,11 @@
 import { html, render } from "../../node_modules/lit-html/lit-html.js";
+import { login } from "../services/requests.js";
 
-const loginTemplate = () => html`
+const loginTemplate = (submitHandler) => html`
 <section id="login">
         <div class="form">
           <h2>Login</h2>
-          <form class="login-form">
+          <form class="login-form" @submit=${submitHandler}>
             <input type="text" name="email" id="email" placeholder="email" />
             <input type="password" name="password" id="password" placeholder="password" />
             <button type="submit">login</button>
@@ -17,5 +18,15 @@ const loginTemplate = () => html`
 `;
 
 export const loginView = (ctx) => {
-    render(loginTemplate(), document.querySelector('main'));
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    let data = Object.fromEntries(new FormData(e.currentTarget));
+
+    login(data)
+    .then(() => {
+      ctx.page.redirect('/');
+    })
+  }
+  render(loginTemplate(submitHandler), document.querySelector('main'));
 }
